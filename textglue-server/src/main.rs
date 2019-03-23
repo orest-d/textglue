a92afd99a78f61241e4ef068a07d89fa853c5bb8
Orest Dubay <orest3.dubay@gmail.com>
2019-03-23 20:07:49 +0100

    cleanup, better error handling in server

--- textglue-server/src/main.rs
+++ textglue-server/src/main.rs
@@ -11,27 +11,44 @@ extern crate futures;
 extern crate textglue_lib;
 
 use textglue_lib::*;
-//use actix_web::{
-//    dev, server, multipart, error, http, App, Error, HttpRequest, HttpResponse, FutureResponse, http::ContentEncoding};
 use actix_web::{
-    dev, error, http, middleware, multipart, server, App, Error, FutureResponse,
+    dev, error, http, multipart, server, App, Error, FutureResponse,
     HttpMessage, HttpRequest, HttpResponse, Json
 };
 
-use std::collections::{HashMap,HashSet};
-
-use futures::future;
 use futures::{Future, Stream};
-
-//use std::cell::Cell;
-
-
-use std::str::FromStr;
-use std::io::prelude::*;
-use std::io;
 use std::io::Write;
-use std::fs::{self,DirEntry,File};
-use std::path::{Path,PathBuf};
+use std::fs::{self, File};
+use std::path::{Path};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[derive(Debug)]
+struct GenericError {
+    details: String
+}
+
+impl GenericError {
+    fn new(msg: &str) -> GenericError {
+        GenericError{details: msg.to_string()}
+    }
+}
+
+fn generic_error(msg: &str) -> GenericError{
+    GenericError::new(msg)
+}
+
+impl std::fmt::Display for GenericError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f,"{}",self.details)
+    }
+}
+
+impl std::error::Error for GenericError {
+    fn description(&self) -> &str {
+        &self.details
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FileRepository{
@@ -53,13 +70,16 @@ impl FileRepository{
         }
     }
 
-    pub fn load(&self) -> std::io::Result<Database>{
+    pub fn load(&self) -> Result<Database>{
         let all_files = files(&self.directory);
         let mut db = Database::new();
         assert!(!self.metadata_file_postfix.ends_with(&self.snippet_file_postfix));
         assert!(!self.document_file_postfix.ends_with(&self.snippet_file_postfix));
         for path in all_files{
-            let filename = Path::new(&path).file_name().expect("File name expected").to_str().expect("Filename err");
+            let filename = Path::new(&path)
+                .file_name()
+                .ok_or(generic_error(&format!("Path error (F): {}",path)))?
+                .to_str().ok_or(generic_error(&format!("Path error (S): {}",path)))?;
             if filename.ends_with(&self.snippet_file_postfix){
                 let id = &filename[..(&filename.len()-self.snippet_file_postfix.len())];
                 db.snippets.insert(id.to_string(),fs::read_to_string(&path)?);
@@ -67,27 +87,32 @@ impl FileRepository{
             else if filename.ends_with(&self.metadata_file_postfix){
                 let id = &filename[..(&filename.len()-self.metadata_file_postfix.len())];
                 let yaml = fs::read_to_string(&path)?;
-                db.metadata.insert(id.to_string(),serde_yaml::from_str(&yaml).expect("Metadata error"));
+                db.metadata.insert(id.to_string(),serde_yaml::from_str(&yaml)?);
             }
             else if filename.ends_with(&self.document_file_postfix){
                 let id = &filename[..(&filename.len()-self.document_file_postfix.len())];
                 let yaml = fs::read_to_string(&path)?;
-                    db.documents.insert(id.to_string(),serde_yaml::from_str(&yaml).expect("Document error"));
+                    db.documents.insert(id.to_string(),serde_yaml::from_str(&yaml)?);
             }
         }
         Ok(db)
     }
 
-    pub fn save(&self,db:&Database) -> std::io::Result<()>{
+    pub fn load_json(&self) -> Result<String>{
+        Ok(serde_json::to_string(&self.load()?)?)
+    }
+
+    pub fn save(&self,db:&Database) -> Result<()>{
         self.save_to_directory(db, &self.directory)
     }
 
-    pub fn save_json(&self,json:&str) {
-        let db:Database = serde_json::from_str(json).unwrap();
-        self.save(&db).unwrap();
+    pub fn save_json(&self,json:&str) -> Result<()>{
+        let db:Database = serde_json::from_str(json)?;
+        self.save(&db)?;
+        Ok(())
     }
 
-    pub fn save_to_directory(&self,db:&Database,directory:&str) -> std::io::Result<()>{
+    pub fn save_to_directory(&self,db:&Database,directory:&str) -> Result<()>{
         let dirpath = Path::new(directory);
         fs::create_dir_all(dirpath)?;
         for (key,value) in db.snippets.iter(){
@@ -98,17 +123,17 @@ impl FileRepository{
         for (key,value) in db.metadata.iter(){
             let path = dirpath.join(format!("{}{}",key,self.metadata_file_postfix));
             let mut file = File::create(&path)?;
-            file.write_all(serde_yaml::to_string(&value).expect("Yaml error").as_bytes())?;
+            file.write_all(serde_yaml::to_string(&value)?.as_bytes())?;
         }
         for (key,value) in db.documents.iter(){
             let path = dirpath.join(format!("{}{}",key,self.document_file_postfix));
             let mut file = File::create(&path)?;
-            file.write_all(serde_yaml::to_string(&value).expect("Yaml error").as_bytes())?;
+            file.write_all(serde_yaml::to_string(&value)?.as_bytes())?;
         }
         Ok(())
     }
 
-    pub fn remove_unused(&self,db:&Database) -> std::io::Result<()>{
+    pub fn remove_unused(&self,db:&Database) -> Result<()>{
         let all_files = files(&self.directory);
         let keys:Vec<String> = db.keys().collect();
         for path in all_files{
@@ -167,7 +192,7 @@ pub fn handle_multipart_item(
         multipart::MultipartItem::Field(field) => {
             Box::new(field
                 .map_err(error::ErrorInternalServerError)
-                .concat()
+                .concat2()
                 .map(|bytes| bytes.to_vec()).into_stream()
             )
         },
@@ -179,21 +204,21 @@ pub fn handle_multipart_item(
     }
 }
 
-pub fn upload<T>(req: HttpRequest<T>) -> FutureResponse<HttpResponse> {
+pub fn upload(req: HttpRequest<FileRepository>) -> FutureResponse<HttpResponse> {
     Box::new(
         req.multipart()
             .map_err(error::ErrorInternalServerError)
             .map(handle_multipart_item)
             .flatten()
             .collect()
-            .map(|content| {
+            .map(move |content| {
                 let b = content.concat();
                 if let Ok(s) = String::from_utf8(b){
                     fs::write("uploaded.json",&s).unwrap();
-                    FileRepository::new().save_json(&s);
+                    req.state().save_json(&s);
                     HttpResponse::Ok().body("Saved")
                 }
-                else{
+                else{                    
                     HttpResponse::InternalServerError().reason("UTF8 decoding error").body("UTF8 decoding error")
                 }
             }
@@ -202,59 +227,96 @@ pub fn upload<T>(req: HttpRequest<T>) -> FutureResponse<HttpResponse> {
 }
 
 fn upload_json(db:Json<Database>) -> HttpResponse{
-    FileRepository::new().save(&db);
+    match FileRepository::new().save(&db){
+        Ok(()) =>{
+            HttpResponse::Ok()
+            .content_type("application/json")
+            .body(&format!("{{\"status\":\"OK\", message:\"OK\"}}"))
+        },
+        Err(e) => {
+            HttpResponse::InternalServerError()
+            .content_type("application/json")
+            .body(&format!("{{\"status\":\"Error\", message:\"{}\"}}",e))
+        }
+    }
 
-    HttpResponse::Ok()
-            .content_type("text/plain")
-            .body("ok")    
 }
+
+fn serve_database(request:&HttpRequest<FileRepository>) -> HttpResponse{
+    let repo:&FileRepository = request.state();
+    match repo.load_json(){
+        Ok(json) => {
+            HttpResponse::Ok()
+            .content_type("application/json")
+            .body(json)
+        },
+        Err(e) =>{
+            HttpResponse::InternalServerError()
+            .content_type("application/json")
+            .body(&format!("{{\"status\":\"Error\", message:\"{}\"}}",e))
+        }
+    }
+}
+
 fn main() {
 //    println!("Hello, world!");
 //    let directory = files(".");
 //    for (i,p) in directory.iter().enumerate(){
 //        println!("{} {:?}",i,p);
 //    }
+/*
     let mut db:Database = FileRepository::new().load().unwrap();
     db.fill().remove_undefined_snippets_from_documents();
     db.document().add_chapter("Introduction").add_snippet("Intro");
     FileRepository::new().save_to_directory(&db, "md1");
-    
-    let s = fs::read_to_string("dbnow.json").unwrap();
-    let db:Database = serde_json::from_str(&s).unwrap();
+*/  
+//    let s = fs::read_to_string("dbnow.json").unwrap();
+//    let db:Database = serde_json::from_str(&s).unwrap();
 //    println!("{}",serde_json::to_string_pretty(&db).unwrap());
     
-    server::new(|| App::with_state(FileRepository::new().load().unwrap())
+    server::new(|| App::with_state(FileRepository::new())
         .resource("/", |r| r.f(index))
-        .resource("/api/db.json", |r| r.f(|r| {
+/*        .resource("/api/db.json", |r| r.f(|r| {
+            r.state.load() match {
+                Ok(&db) =>{
+
+                } r.state.load(){
+                
+                HttpResponse::Ok()
+                .content_type("application/json")
+                .body(db.to_json().unwrap())
+
+            }
             HttpResponse::Ok()
             .content_type("application/json")
             .body(r.state().to_json().unwrap())
-        }))
-        .resource("/api/dbnow.json", |r| r.f(|r| {
+        }))*/
+        .resource("/api/db.json", |r| r.f(serve_database))
+        .resource("/api/dbnow.json", |r| r.f(|_r| {
             HttpResponse::Ok()
             .content_type("application/json")
             .body(FileRepository::new().load().unwrap().to_json().unwrap())
         }))
-        .resource("/index.html", |r| r.f(|r| {
-            const content: &'static [u8] = include_bytes!("../../textglue-wasm/www/app.html");
+        .resource("/index.html", |r| r.f(|_r| {
+            const CONTENT: &'static [u8] = include_bytes!("../../textglue-wasm/www/app.html");
             HttpResponse::Ok()
             .content_type("text/html")
-            .body(content)
+            .body(CONTENT)
         }))
-        .resource("/textglue_wasm.js", |r| r.f(|r| {
-            const content: &'static [u8] = include_bytes!("../../textglue-wasm/pkg/textglue_wasm.js");
+        .resource("/textglue_wasm.js", |r| r.f(|_r| {
+            const CONTENT: &'static [u8] = include_bytes!("../../textglue-wasm/pkg/textglue_wasm.js");
             HttpResponse::Ok()
             .content_type("application/javascript")
-            .body(content)
+            .body(CONTENT)
         }))
-        .resource("/textglue_wasm_bg.wasm", |r| r.f(|r| {
-            const content: &'static [u8] = include_bytes!("../../textglue-wasm/pkg/textglue_wasm_bg.wasm");
+        .resource("/textglue_wasm_bg.wasm", |r| r.f(|_r| {
+            const CONTENT: &'static [u8] = include_bytes!("../../textglue-wasm/pkg/textglue_wasm_bg.wasm");
             HttpResponse::Ok()
             .content_type("application/wasm")
-            .body(content)
+            .body(CONTENT)
         }))
         .resource("/api/upload", |r| {
-                r.method(http::Method::GET).f(|r| {"Upload"});
+                r.method(http::Method::GET).f(|_r| {"Upload"});
                 r.method(http::Method::POST).with(upload);
         })
         .resource("/api/upload-json", |r|
