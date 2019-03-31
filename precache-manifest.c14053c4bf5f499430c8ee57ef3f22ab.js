a771ccd7a0fc59b9f8efc7a593a01a2419bbafad
Orest Dubay <orest3.dubay@gmail.com>
2019-04-01 00:02:24 +0200

    Adding webapp

---
+++ precache-manifest.c14053c4bf5f499430c8ee57ef3f22ab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "54c6e38f060ed915479f4ab3f7810c2e",
+    "url": "/api/db.json"
+  },
+  {
+    "revision": "83546107d333b4ae10e4c074d14137a1",
+    "url": "/textglue_wasm_bg.wasm"
+  },
+  {
+    "revision": "23e4d9de06bde5c3eb309ea29fb9724d",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "11365b709fa6557eb31a",
+    "url": "/js/about.891ba4a8.js"
+  },
+  {
+    "revision": "deb528b5acecafd58c361526bd04ad58",
+    "url": "/textglue_wasm.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c5b94da2c46d0229c54c",
+    "url": "/js/app.990bc430.js"
+  },
+  {
+    "revision": "74b1615568445cbc2a30",
+    "url": "/js/chunk-vendors.6de60966.js"
+  },
+  {
+    "revision": "23e4d9de06bde5c3eb309ea29fb9724d",
+    "url": "/logo1.png"
+  },
+  {
+    "revision": "c99192202833eaa48cea9d430dba6f75",
+    "url": "/logo2.png"
+  },
+  {
+    "revision": "fc584600e09e5a44d07b681dd8b9cf83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "74b1615568445cbc2a30",
+    "url": "/css/chunk-vendors.07546c14.css"
+  }
+];
